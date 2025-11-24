8722e44602a2956844ebe543c6136b5fa77e28d9
NazrulNazir <nazrul6290@gmail.com>
2025-11-24 22:54:31 +0600

    all commit

--- src/components/ModuleOne/ModuleOne.jsx
+++ src/components/ModuleOne/ModuleOne.jsx
@@ -2,14 +2,11 @@ import { Link } from "react-router"
 
 const ModuleOne = () => {
   return (
-    <div className='flex flex-col justify-center items-center h-[100vh]'>
-      <h1 className='text-info text-4xl md:text-6xl text-center font-bold' id='text-shadow'>Will you be my life <br />Partner ?</h1>
+    <div className='flex flex-col justify-center items-center h-[100vh] px-7'>
+      <h1 className='text-info text-4xl md:text-6xl text-center font-bold' id='text-shadow'>Truly, today is a special day.🎈</h1>
       <div className='flex justify-center gap-6 md:gap-10 mt-20'>
         <Link to={'/love'}>
-          <button className='bg-warning  px-12 md:px-18 py-1.5 rounded-xl font-bold text-xs md:text-xl text-black hover:text-black hover:bg-amber-400 cursor-pointer'>Yes</button>
-        </Link>
-        <Link to={'/love'}>
-          <button className='bg-warning px-3 py-1.5 rounded-xl font-bold text-xs md:text-xl text-black shadow-red hover:bg-amber-400 hover:text-black cursor-pointer'>Of course Yes!!!</button>
+          <button className='bg-warning  px-6 md:px-18 py-1.5 rounded-xl font-bold text-xs md:text-xl text-black hover:text-black hover:bg-amber-400 cursor-pointer'>Unlock Today</button>
         </Link>
       </div>
     </div>
--- src/components/ModuleTwo/ModuleTwo.jsx
+++ src/components/ModuleTwo/ModuleTwo.jsx
@@ -2,8 +2,13 @@ import { Link } from "react-router"
 
 const ModuleTwo = () => {
   return (
-    <div className='flex justify-center items-center h-[90vh]' id='love-shado'>
-        <h1 className='text-accent text-5xl font-bold md:text-6xl'>I love you..💕</h1>
+    <div className='flex justify-center flex-col items-center h-[90vh] px-6' id='love-shado'>
+      <div className="text-3xl text-indigo-200 animate-bounce"><p className="text-center">Wishing you a </p> <p className="text-center">Birthday that's as</p> <p className="text-center">wonderful and amazing</p> <p className="text-center">as you are.🎉</p></div>
+        <div className="">
+          <h1 className='text-accent text-3xl font-bold md:text-6xl mt-10'>🎈🎂Happy Birthday,</h1>
+          <h1 className='text-accent text-3xl font-bold md:text-6xl mt-4'>Sharmin..💕🎉</h1>
+        </div>
+        <img src="" alt="" />
         <Link to={'/'}>
           <button className='absolute top-5 left-5 bg-indigo-400 text-base-300  font-bold px-5 py-1 rounded-lg cursor-poiter'>Back</button>
         </Link>
